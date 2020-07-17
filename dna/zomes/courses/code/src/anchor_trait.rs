8950df7069cf29ee7995fe4744839fbd23424dca
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2020-07-17 18:02:37 +0300

    dna::course: AnchorTrait definition from Hedayat

---
+++ dna/zomes/courses/code/src/anchor_trait.rs
@@ -0,0 +1,17 @@
+use hdk::prelude::*;
+use std::convert::TryFrom;
+
+// using Hedayat's code here
+pub trait AnchorTrait: TryFrom<JsonString> + Into<JsonString> + Clone {
+    fn entry_type() -> String;
+    fn link_to() -> String;
+    fn link_type() -> String;
+    fn entry(self) -> Entry {
+        Entry::App(Self::entry_type().into(), self.into())
+    }
+
+    fn address(&self) -> ZomeApiResult<Address> {
+        let addrss = hdk::commit_entry(&self.clone().entry())?;
+        return Ok(addrss);
+    }
+}
